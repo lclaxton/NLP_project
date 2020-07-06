6c7b18387faad7caa871b4f581395fb4f8c20297
lclaxton <54288507+lclaxton@users.noreply.github.com>
2020-07-06 22:29:33 +0100

    Changed to manual again
    Changed it back to manual and started to add functunality to visualise values. Also enabled the search to look at a combination of two words, rather than just one.

--- NLP_processor.py
+++ NLP_processor.py
@@ -20,43 +20,43 @@ import string
 def remove_punctuation(text):
     no_punct = "".join([c for c in text if c not in string.punctuation])
     return no_punct
+df.iloc[:,5] = df.iloc[:,5].apply(lambda x: remove_punctuation(x.lower()))
 
-'''
-Heres how to tokenise it manually 
-# Now to tokenize the words
+
+# Now to tokenize the words to enable stopword removal
 from nltk.tokenize import RegexpTokenizer
 
 tokenizer = RegexpTokenizer(r'\w+')
-
 df.iloc[:,5] = df.iloc[:,5].apply(lambda x: tokenizer.tokenize(x.lower()))
 
 # Now to remove common stopwords
+from nltk.corpus import stopwords
 def remove_stopwords(text):
     words = [w for w in text if w not in stopwords.words('english')]
     return words
 
 df.iloc[:,5] = df.iloc[:,5].apply(lambda x: remove_stopwords(x))
-'''
 
-from nltk.corpus import stopwords
+# Now to recombine for analysis
+df.iloc[:,5] = df.iloc[:,5].apply(lambda x:" ".join(x))
 
-# Vectorisation
-def allinone(text):
-    new = text.split("\n")
-    new = ' '.join(new)
-    new = "".join([c for c in new if c not in string.punctuation])
-    new = [w for w in new.split() if w.lower() not in stopwords.words('english')]
-    return new
-    
-    
+
+# Begin vectorisation
 from sklearn.feature_extraction.text import CountVectorizer
 
 # Might take awhile...
-bow_transformer = CountVectorizer(analyzer=allinone).fit(df.iloc[:,5])
+max_feature_length = 10
+bow_transformer = CountVectorizer(max_features=max_feature_length,ngram_range=(1,2)).fit(df.iloc[:,5])
 
-# Print total number of vocab words
-print(len(bow_transformer.vocabulary_))
+# need to figure out how to get total counts 
+
+# below is counts from one entry 
 
 bow = bow_transformer.transform([df.iloc[0,5]])
-print(bow)
-print(bow_transformer.get_feature_names()[95])
+print(bow) 
+
+word_list = bow_transformer.get_feature_names()
+count_list = bow.toarray().sum(axis=0) 
+top_counts = pd.DataFrame(zip(word_list,count_list),columns=['term','count',])
+top_counts.sort_values('count',axis=0,inplace=True, ascending=False)
+print(top_counts)
