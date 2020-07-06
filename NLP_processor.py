2cca5b0bb84dd0bd0ebf33a604243ceee72b32e3
lclaxton <54288507+lclaxton@users.noreply.github.com>
2020-07-06 21:02:08 +0100

    Added automatic vectorisation
    Changed the script from manually tokenising the data to using CountVectoriser() to do it automatically

--- NLP_processor.py
+++ NLP_processor.py
@@ -21,6 +21,8 @@ def remove_punctuation(text):
     no_punct = "".join([c for c in text if c not in string.punctuation])
     return no_punct
 
+'''
+Heres how to tokenise it manually 
 # Now to tokenize the words
 from nltk.tokenize import RegexpTokenizer
 
@@ -28,12 +30,33 @@ tokenizer = RegexpTokenizer(r'\w+')
 
 df.iloc[:,5] = df.iloc[:,5].apply(lambda x: tokenizer.tokenize(x.lower()))
 
-
 # Now to remove common stopwords
-from nltk.corpus import stopwords
-
 def remove_stopwords(text):
     words = [w for w in text if w not in stopwords.words('english')]
     return words
 
 df.iloc[:,5] = df.iloc[:,5].apply(lambda x: remove_stopwords(x))
+'''
+
+from nltk.corpus import stopwords
+
+# Vectorisation
+def allinone(text):
+    new = text.split("\n")
+    new = ' '.join(new)
+    new = "".join([c for c in new if c not in string.punctuation])
+    new = [w for w in new.split() if w.lower() not in stopwords.words('english')]
+    return new
+    
+    
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Might take awhile...
+bow_transformer = CountVectorizer(analyzer=allinone).fit(df.iloc[:,5])
+
+# Print total number of vocab words
+print(len(bow_transformer.vocabulary_))
+
+bow = bow_transformer.transform([df.iloc[0,5]])
+print(bow)
+print(bow_transformer.get_feature_names()[95])
